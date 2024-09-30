15cdac83340613d90f94f4648e8d1100db5e854f
Ozan YAPRAK <ozanerhanyaprak@icloud.com>
2024-09-30 16:27:32 +0300

    #

--- FormProduct.Designer.cs
+++ FormProduct.Designer.cs
@@ -32,179 +32,213 @@
             this.btnSearch = new System.Windows.Forms.Button();
             this.btnUpdate = new System.Windows.Forms.Button();
             this.btnDelete = new System.Windows.Forms.Button();
-            this.textBoxCategoryName = new System.Windows.Forms.TextBox();
+            this.textBoxProductName = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.textBoxCategoryId = new System.Windows.Forms.TextBox();
+            this.textBoxProductId = new System.Windows.Forms.TextBox();
             this.btnCreate = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBoxProductPrice = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBoxProductStock = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBoxProductCategory = new System.Windows.Forms.ComboBox();
+            this.btnProductListWithCategory = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // btnList
             // 
-            this.btnList.Location = new System.Drawing.Point(77, 321);
+            this.btnList.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.btnList.Location = new System.Drawing.Point(4, 176);
             this.btnList.Name = "btnList";
-            this.btnList.Size = new System.Drawing.Size(161, 32);
+            this.btnList.Size = new System.Drawing.Size(125, 32);
             this.btnList.TabIndex = 19;
             this.btnList.Text = "Listele";
-            this.btnList.UseVisualStyleBackColor = true;
+            this.btnList.UseVisualStyleBackColor = false;
+            this.btnList.Click += new System.EventHandler(this.btnList_Click);
             // 
             // btnSearch
             // 
-            this.btnSearch.Location = new System.Drawing.Point(77, 473);
+            this.btnSearch.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.btnSearch.Location = new System.Drawing.Point(4, 328);
             this.btnSearch.Name = "btnSearch";
-            this.btnSearch.Size = new System.Drawing.Size(161, 32);
+            this.btnSearch.Size = new System.Drawing.Size(262, 32);
             this.btnSearch.TabIndex = 18;
             this.btnSearch.Text = "Ara";
-            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.UseVisualStyleBackColor = false;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
             // 
             // btnUpdate
             // 
-            this.btnUpdate.Location = new System.Drawing.Point(77, 435);
+            this.btnUpdate.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.btnUpdate.Location = new System.Drawing.Point(4, 290);
             this.btnUpdate.Name = "btnUpdate";
-            this.btnUpdate.Size = new System.Drawing.Size(161, 32);
+            this.btnUpdate.Size = new System.Drawing.Size(262, 32);
             this.btnUpdate.TabIndex = 17;
             this.btnUpdate.Text = "Guncelle";
-            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.UseVisualStyleBackColor = false;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
             // 
             // btnDelete
             // 
-            this.btnDelete.Location = new System.Drawing.Point(77, 397);
+            this.btnDelete.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.btnDelete.Location = new System.Drawing.Point(4, 252);
             this.btnDelete.Name = "btnDelete";
-            this.btnDelete.Size = new System.Drawing.Size(161, 32);
+            this.btnDelete.Size = new System.Drawing.Size(262, 32);
             this.btnDelete.TabIndex = 16;
             this.btnDelete.Text = "Sil";
-            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.UseVisualStyleBackColor = false;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
             // 
-            // textBoxCategoryName
+            // textBoxProductName
             // 
-            this.textBoxCategoryName.Location = new System.Drawing.Point(77, 35);
-            this.textBoxCategoryName.Name = "textBoxCategoryName";
-            this.textBoxCategoryName.Size = new System.Drawing.Size(161, 20);
-            this.textBoxCategoryName.TabIndex = 15;
+            this.textBoxProductName.Location = new System.Drawing.Point(82, 35);
+            this.textBoxProductName.Name = "textBoxProductName";
+            this.textBoxProductName.Size = new System.Drawing.Size(183, 20);
+            this.textBoxProductName.TabIndex = 15;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(5, 38);
+            this.label2.Location = new System.Drawing.Point(25, 39);
             this.label2.Name = "label2";
+            this.label2.RightToLeft = System.Windows.Forms.RightToLeft.No;
             this.label2.Size = new System.Drawing.Size(54, 13);
             this.label2.TabIndex = 14;
             this.label2.Text = "Ürün Adi :";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // dataGridView1
             // 
             this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(244, 12);
+            this.dataGridView1.Location = new System.Drawing.Point(272, 12);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(522, 244);
+            this.dataGridView1.Size = new System.Drawing.Size(764, 348);
             this.dataGridView1.TabIndex = 13;
             // 
-            // textBoxCategoryId
+            // textBoxProductId
             // 
-            this.textBoxCategoryId.Location = new System.Drawing.Point(77, 12);
-            this.textBoxCategoryId.Name = "textBoxCategoryId";
-            this.textBoxCategoryId.Size = new System.Drawing.Size(161, 20);
-            this.textBoxCategoryId.TabIndex = 12;
+            this.textBoxProductId.Location = new System.Drawing.Point(83, 12);
+            this.textBoxProductId.Name = "textBoxProductId";
+            this.textBoxProductId.Size = new System.Drawing.Size(183, 20);
+            this.textBoxProductId.TabIndex = 12;
             // 
             // btnCreate
             // 
-            this.btnCreate.Location = new System.Drawing.Point(77, 359);
+            this.btnCreate.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.btnCreate.Location = new System.Drawing.Point(4, 214);
             this.btnCreate.Name = "btnCreate";
-            this.btnCreate.Size = new System.Drawing.Size(161, 32);
+            this.btnCreate.Size = new System.Drawing.Size(262, 32);
             this.btnCreate.TabIndex = 11;
             this.btnCreate.Text = "Ekle";
-            this.btnCreate.UseVisualStyleBackColor = true;
+            this.btnCreate.UseVisualStyleBackColor = false;
+            this.btnCreate.Click += new System.EventHandler(this.btnCreate_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(9, 15);
+            this.label1.Location = new System.Drawing.Point(29, 16);
             this.label1.Name = "label1";
+            this.label1.RightToLeft = System.Windows.Forms.RightToLeft.No;
             this.label1.Size = new System.Drawing.Size(50, 13);
             this.label1.TabIndex = 10;
             this.label1.Text = "Ürün ID :";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
-            // textBox1
+            // textBoxProductPrice
             // 
-            this.textBox1.Location = new System.Drawing.Point(77, 61);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(161, 20);
-            this.textBox1.TabIndex = 21;
+            this.textBoxProductPrice.Location = new System.Drawing.Point(83, 61);
+            this.textBoxProductPrice.Name = "textBoxProductPrice";
+            this.textBoxProductPrice.Size = new System.Drawing.Size(183, 20);
+            this.textBoxProductPrice.TabIndex = 21;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(5, 64);
+            this.label3.Location = new System.Drawing.Point(18, 65);
             this.label3.Name = "label3";
+            this.label3.RightToLeft = System.Windows.Forms.RightToLeft.No;
             this.label3.Size = new System.Drawing.Size(61, 13);
             this.label3.TabIndex = 20;
             this.label3.Text = "Ürün Fiyat :";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
-            // textBox2
+            // textBoxProductStock
             // 
-            this.textBox2.Location = new System.Drawing.Point(77, 87);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(161, 20);
-            this.textBox2.TabIndex = 23;
+            this.textBoxProductStock.Location = new System.Drawing.Point(83, 87);
+            this.textBoxProductStock.Name = "textBoxProductStock";
+            this.textBoxProductStock.Size = new System.Drawing.Size(183, 20);
+            this.textBoxProductStock.TabIndex = 23;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(5, 90);
+            this.label4.Location = new System.Drawing.Point(18, 91);
             this.label4.Name = "label4";
+            this.label4.RightToLeft = System.Windows.Forms.RightToLeft.No;
             this.label4.Size = new System.Drawing.Size(61, 13);
             this.label4.TabIndex = 22;
             this.label4.Text = "Ürün Stok :";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(5, 116);
+            this.label5.Location = new System.Drawing.Point(1, 120);
             this.label5.Name = "label5";
+            this.label5.RightToLeft = System.Windows.Forms.RightToLeft.No;
             this.label5.Size = new System.Drawing.Size(78, 13);
             this.label5.TabIndex = 24;
             this.label5.Text = "Ürün Kategori :";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
-            // comboBox1
+            // comboBoxProductCategory
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(77, 116);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(161, 21);
-            this.comboBox1.TabIndex = 26;
+            this.comboBoxProductCategory.FormattingEnabled = true;
+            this.comboBoxProductCategory.Location = new System.Drawing.Point(83, 116);
+            this.comboBoxProductCategory.Name = "comboBoxProductCategory";
+            this.comboBoxProductCategory.Size = new System.Drawing.Size(183, 21);
+            this.comboBoxProductCategory.TabIndex = 26;
+            // 
+            // btnProductListWithCategory
+            // 
+            this.btnProductListWithCategory.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.btnProductListWithCategory.Location = new System.Drawing.Point(135, 176);
+            this.btnProductListWithCategory.Name = "btnProductListWithCategory";
+            this.btnProductListWithCategory.Size = new System.Drawing.Size(130, 32);
+            this.btnProductListWithCategory.TabIndex = 27;
+            this.btnProductListWithCategory.Text = "Listele";
+            this.btnProductListWithCategory.UseVisualStyleBackColor = false;
+            this.btnProductListWithCategory.Click += new System.EventHandler(this.btnProductListWithCategory_Click);
             // 
             // FormProduct
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(779, 655);
-            this.Controls.Add(this.comboBox1);
+            this.ClientSize = new System.Drawing.Size(1048, 371);
+            this.Controls.Add(this.btnProductListWithCategory);
+            this.Controls.Add(this.comboBoxProductCategory);
             this.Controls.Add(this.label5);
-            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBoxProductStock);
             this.Controls.Add(this.label4);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.textBoxProductPrice);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.btnList);
             this.Controls.Add(this.btnSearch);
             this.Controls.Add(this.btnUpdate);
             this.Controls.Add(this.btnDelete);
-            this.Controls.Add(this.textBoxCategoryName);
+            this.Controls.Add(this.textBoxProductName);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.dataGridView1);
-            this.Controls.Add(this.textBoxCategoryId);
+            this.Controls.Add(this.textBoxProductId);
             this.Controls.Add(this.btnCreate);
             this.Controls.Add(this.label1);
             this.Name = "FormProduct";
             this.Text = "FormProduct";
+            this.Load += new System.EventHandler(this.FormProduct_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -217,17 +251,18 @@
         private System.Windows.Forms.Button btnSearch;
         private System.Windows.Forms.Button btnUpdate;
         private System.Windows.Forms.Button btnDelete;
-        private System.Windows.Forms.TextBox textBoxCategoryName;
+        private System.Windows.Forms.TextBox textBoxProductName;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.TextBox textBoxCategoryId;
+        private System.Windows.Forms.TextBox textBoxProductId;
         private System.Windows.Forms.Button btnCreate;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBoxProductPrice;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBoxProductStock;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBoxProductCategory;
+        private System.Windows.Forms.Button btnProductListWithCategory;
     }
 }
--- FormProduct.cs
+++ FormProduct.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.Entity;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,9 +14,100 @@ namespace EntityFrameworkDbFirstProject
 {
     public partial class FormProduct : Form
     {
+        DbFirstDbEntities _dbContext = new DbFirstDbEntities();
+
+
         public FormProduct()
         {
             InitializeComponent();
+        }
+
+
+        public void ProductList()
+        {
+            var products = _dbContext.Product.ToList();
+            dataGridView1.DataSource = products;
+        }
+
+
+        private void FormProduct_Load(object sender, EventArgs e)
+        {
+            var categories = _dbContext.Category.ToList();
+
+            comboBoxProductCategory.DisplayMember = "Name";
+            comboBoxProductCategory.ValueMember = "Id";
+
+            comboBoxProductCategory.DataSource = categories;
+        }
+
+
+        private void btnList_Click(object sender, EventArgs e)
+        {
+            ProductList();
+        }
+
+        private void btnCreate_Click(object sender, EventArgs e)
+        {
+            Product product = new Product();
+
+            product.Name = textBoxProductName.Text;
+            product.Price = textBoxProductPrice.Text;
+            product.Stock = Int32.Parse(textBoxProductStock.Text);
+            product.CategoryId = (int)comboBoxProductCategory.SelectedValue;
+
+            _dbContext.Product.Add(product);
+            _dbContext.SaveChanges();
+
+            ProductList();
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            int id = Int32.Parse(textBoxProductId.Text);
+            var product = _dbContext.Product.Find(id);
+
+            _dbContext.Product.Remove(product);
+            _dbContext.SaveChanges();
+
+            ProductList();
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            int id = Convert.ToInt32(textBoxProductId.Text);
+            var product = _dbContext.Product.Find(id);
+
+            product.Name = textBoxProductName.Text;
+            product.Price = textBoxProductPrice.Text;
+            product.Stock = Int32.Parse(textBoxProductStock.Text);
+            product.CategoryId = (int)comboBoxProductCategory.SelectedValue;
+
+            _dbContext.SaveChanges();
+
+            ProductList();
+        }
+
+        private void btnProductListWithCategory_Click(object sender, EventArgs e)
+        {
+            var values = _dbContext.Product.Join(_dbContext.Category, product => product.CategoryId, category => category.Id, (product, category) => new
+            {
+                ProductName = product.Name,
+                ProductId = product.Id,
+                ProductPrice = product.Price,
+                ProductStock = product.Stock,
+                CategoryId = category.Id,
+                CategoryName = category.Name,
+
+            }).ToList();
+
+            dataGridView1.DataSource = values;
+
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            var searchValues = _dbContext.Product.Where(x => x.Name == textBoxProductName.Text).ToList();
+            dataGridView1.DataSource = searchValues;
         }
     }
 }
